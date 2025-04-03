57efe13775f028d5a9f40dbfe0a9424165b5eddb
Jonathan Cook <jcookbme@gmail.com>
2025-04-03 09:11:18 -0400

    basic routing

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+};
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
 import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({});
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./style/theme";
+import Layout from "./components/navigation/Layout";
+import HomePage from "./pages/home/HomePage";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div>Hello World</div>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
